0f7f74dbc3397654278453d3167ef8ac5737731d
Varvara <sadchikhina@gmail.com>
2018-02-19 11:51:25 +0300

    modal-window-complete

--- js/modal.js
+++ js/modal.js
@@ -1,7 +1,20 @@
 var modalWindow = document.querySelector('.modal-window');
 var modalClose = modalWindow.querySelector('.close-button');
 var modalOpen = document.querySelector('.feedback-button');
+var ESC_KEYCODE = 27
 
-modalOpen.addEventListener('click', function () {
-  modalWindow.classList.remove('hidden');
+modalOpen.addEventListener('click', function (evt) {
+  evt.preventDefault();
+  modalWindow.classList.add('modal-show');
+});
+
+modalClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modalWindow.classList.remove('modal-show');
+});
+
+document.addEventListener('keydown', function (evt) {
+if (evt.keyCode === ESC_KEYCODE) {
+        modalWindow.classList.remove('modal-show');
+  }
 });
